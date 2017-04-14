7f44c7a292c08404f7976a66ac0b86bb6ac87509
Alex Batista <alexbatista.asb@gmail.com>
2017-04-14 20:52:02 -0300

    First Commit

---
+++ App/js/app.js
@@ -0,0 +1,12 @@
+const BrowserWindow  = require('electron').remote.BrowserWindow
+const path = require('path')
+
+const newWindowBtn = document.getElementById('new-window')
+
+newWindowBtn.addEventListener('click',function(event){
+  const modalPath = path.join('file://',__dirname,'../modal.html')
+  let win = new BrowserWindow({width: 800, height: 600})
+  win.on('close',function(){win = null})
+  win.loadURL(modalPath)
+  win.show()
+})
